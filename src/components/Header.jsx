77c288a61de4df49a05ff5ce8fba0a65fbd021e3
Ugo Capelle <capelleu@icloud.com>
2024-12-01 15:37:04 +0100

    changements logo header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Toggle from "./Toggle";
 import "../styles/Header.css";
-
-import githubBlanc from "../assets/githubblanc.png";
-import githubNoir from "../assets/githubnoir.png";
+import logoBlanc from "../assets/UCBLANC.png";
+import logoNoir from "../assets/UCNOIR.png";
 
 function Header({ isDarkMode, toggleDarkMode }) {
   return (
     <header className={`header ${isDarkMode ? "dark-mode" : "light-mode"}`}>
       <Link to="/" className="title-link">
-        <h1 className="title">Ugo Capelle</h1>
+        <img
+          src={isDarkMode ? logoBlanc : logoNoir}
+          alt="Ugo Capelle Logo"
+          className="logo"
+        />
       </Link>
-      <div className="right-section">
-        <a
-          href="https://github.com/UgoCapelle"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="github-link-header"
-        >
-          GitHub
-          <img
-            src={isDarkMode ? githubBlanc : githubNoir}
-            alt="GitHub Logo"
-            className="github-icon"
-          />
-        </a>
-        <Toggle isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
-      </div>
+      <Toggle isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
     </header>
   );
 }
